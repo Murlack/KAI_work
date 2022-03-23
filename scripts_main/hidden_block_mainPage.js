67c23b78e8aecab5340860a9fc28d4bf4357bb79
timur <timur.zagid@bk.ru>
2022-03-23 19:29:51 +0300

    добавил скрипты для отоброжение меню в двух элементах

--- scripts_main/hidden_block_mainPage.js
+++ scripts_main/hidden_block_mainPage.js
@@ -1,15 +1,39 @@
+let menuHiddenButtom = document.querySelector(".menu_links_box_displayWhere700px");
+let Elem = document.querySelector(".menu_links_boxs_links");
+let hideMenuInProffil = document.querySelector(".buttonsInHeader_box_usButton");
+var none = 'none';
+var flex = 'flex';
 
-function hiddenMainPage_butTopMenu(HiddenMainPage_butTopMenuVar){
-    HiddenMainPage_butTopMenuVar.style.display = DisplayStyleCheck(HiddenMainPage_butTopMenuVar);
-}
-function DisplayStyleCheck(HiddenMainPage_butTopMenuVar){
-    let p1 = HiddenMainPage_butTopMenuVar.style.display;
-    if(p1 == 'none'){
-        console.log(p1+'- flex');
-        return 'flex';
+menuHiddenButtom.addEventListener('click',()=>FuncClickDisplayStyleCheck(Elem));
+window.addEventListener('resize',()=>CheckWindowSize(Elem))
+hideMenuInProffil.addEventListener('click',OpenMenu);
+
+function OpenMenu(){
+    let elem = document.querySelector(".hideMenu_in_Proffil");
+    let styleDisplay = window.getComputedStyle(elem,null).getPropertyValue("display");
+    if (styleDisplay == none){
+        elem.style.display = flex;
     }
-    else{
-        console.log(p1+'- none');
-        return 'none';
+    else if (styleDisplay == flex){
+        elem.style.display = none;
+    }
+}
+function CheckWindowSize(elem){
+    if(window.innerWidth == 1062 || window.innerWidth < 1062){
+        elem.style.display=none;
+    }
+    if(window.innerWidth == 1063 || window.innerWidth > 1062){
+        elem.style.display=flex;
+    }
+}
+function FuncClickDisplayStyleCheck(elem){
+    // getComputedStyle позволяет получить объект стилей элемента
+    
+    styleDisplay = window.getComputedStyle(elem,null).getPropertyValue('display');
+    if (styleDisplay == none){
+        elem.style.display = flex;
+    }
+    else if (styleDisplay == flex){
+        elem.style.display = none;
     }
 }
